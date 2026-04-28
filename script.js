b6a8503b5b73230e7bf6d543697d85d0b1c9128f
MDJ-NL <mdjleader@gmail.com>
2026-04-28 09:47:43 +0200

    Added basic functionality for sub20% status alerts

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ const happinessContainer = document.getElementById('happinessWrapper');
 const hungerBar = document.getElementById('hungerBar');
 const energyBar = document.getElementById('energyBar');
 const happinessBar = document.getElementById('happinessBar');
+const green = '#a3db3a';
+const yellow = '#d4bd6f';
+const red = '#fa6f6a';
 
 // main window
 const mainBG = document.getElementById('homeSection')
@@ -19,19 +22,43 @@ const btnLeft = document.getElementById('previousRoom');
 const btnCenter = document.getElementById('selectButton');
 const btnRight = document.getElementById('nextRoom');
 
-/*
+// status alerts
+const bubbleWrapper = document.createElement('div'); // can addded in HTML
+bubbleWrapper.setAttribute('id', 'bubbleWrapper');
+mainBG.appendChild(bubbleWrapper);
+
+const hungryBubble = document.createElement('div');
+const tiredBubble = document.createElement('div');
+const unhappyBubble = document.createElement('div');
+
+hungryBubble.setAttribute('class', 'alertBubble hungry');
+hungryBubble.innerHTML = 'Feed me!';
+
+tiredBubble.setAttribute('class', 'alertBubble tired');
+tiredBubble.innerHTML = 'I need rest!';
+
+unhappyBubble.setAttribute('class', 'alertBubble unhappy');
+unhappyBubble.innerHTML = 'Play with me!';
+
+/* selected room
 1 = hunger
 2 = energy
 3 = happiness */
 let currentRoom = 1;
+const feedRoom = '#adccdf';
+const bedRoom = '#addb79';
+const playRoom = '#e6b3cc';
 
-// creatures 
-let petSpecies = ['cat', 'dog'] // more added later?
+// creatures (unused currently)
+let petSpecies = ['cat', 'dog', 'bunny'] // more added later?
 
 let pet = {
     hunger: 80,
     energy: 80,
-    happiness: 80
+    happiness: 80,
+    hungry: false,
+    tired: false,
+    unhappy: false
 }
 
 /* ======================
@@ -50,17 +77,17 @@ const checkSelection = () => { // add selection to status bars as well
 
     // mark current selected room
     if (currentRoom === 1) { // hunger
-        mainBG.style.background = "#adccdf";
+        mainBG.style.background = feedRoom;
         hungerContainer.style.border = '2px solid #aaee7c';
         hungerContainer.style.backgroundColor = '#d0ff0065';
 
     } else if (currentRoom === 2) { // energy
-        mainBG.style.background = "#addb79";
+        mainBG.style.background = bedRoom;
         energyContainer.style.border = '2px solid #aaee7c';
         energyContainer.style.backgroundColor = '#d0ff0065';
 
     } else if (currentRoom === 3) { // happiness
-        mainBG.style.background = "#e6b3cc";
+        mainBG.style.background = playRoom;
         happinessContainer.style.border = '2px solid #aaee7c';
         happinessContainer.style.backgroundColor = '#d0ff0065';
 
@@ -74,53 +101,75 @@ const checkSelection = () => { // add selection to status bars as well
 ============================== */
 const updateHungerBar = () => {
     hungerBar.style.width = pet.hunger + '%';
+    pet.hungry = false;
 
     if (pet.hunger > 50) {
-        hungerBar.style.background = '#a3db3a'
-    } else if (pet.hunger > 25) {
-        hungerBar.style.backgroundColor = '#d1ad37'
+        hungerBar.style.background = green;
+    } else if (pet.hunger > 20) {
+        hungerBar.style.backgroundColor = yellow;
     } else {
-        hungerBar.style.backgroundColor = '#fc4d47'
+        hungerBar.style.backgroundColor = red;
+        pet.hungry = true;
     }
-
+    
     if (pet.hunger <= 0) {
         pet.hunger = 0;
+    }
+
+    if (pet.hungry) {
+        bubbleWrapper.appendChild(hungryBubble);
+        } else {
+            hungryBubble.remove();
     }
 }
 
 const updateEnergyBar = () => {
     energyBar.style.width = pet.energy + '%';
+    pet.tired = false;
 
     if (pet.energy > 50) {
-        energyBar.style.background = '#a3db3a'
-    } else if (pet.hunger > 25) {
-        energyBar.style.backgroundColor = '#d1ad37'
+        energyBar.style.background = green;
+    } else if (pet.energy > 20) {
+        energyBar.style.backgroundColor = yellow;
     } else {
-        energyBar.style.backgroundColor = '#fc4d47'
+        energyBar.style.backgroundColor = red;
+        pet.tired = true;
     }
 
     if (pet.energy <= 0) {
         pet.energy = 0;
     }
+
+    if (pet.tired) {
+        bubbleWrapper.appendChild(tiredBubble);
+        } else {
+            tiredBubble.remove();
+    }
 }
 
 const updateHappinessBar = () => {
     happinessBar.style.width = pet.happiness + '%';
+    pet.unhappy = false;
 
     if (pet.happiness > 50) {
-        happinessBar.style.background = '#a3db3a'
-    } else if (pet.happiness > 25) {
-        happinessBar.style.backgroundColor = '#d1ad37'
+        happinessBar.style.background = green;
+    } else if (pet.happiness > 20) {
+        happinessBar.style.backgroundColor = yellow;
     } else {
-        happinessBar.style.backgroundColor = '#fc4d47'
+        happinessBar.style.backgroundColor = red;
+        pet.unhappy = true;
     }
 
     if (pet.happiness <= 0) {
         pet.happiness = 0;
     }
+
+    if (pet.unhappy) {
+        bubbleWrapper.appendChild(unhappyBubble);
+        } else {
+            unhappyBubble.remove();
+    }
 }
-
-
 
 const updateStatusbars = () => {
     updateHungerBar();
@@ -193,7 +242,7 @@ const init = () => {
         pet.energy -= 5;
         pet.happiness -= 5;
         updateStatusbars();
-    }, 2500);
+    }, 1000);
 }
 
 init();
