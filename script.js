8de7530ca1830a6da9a35035a37757e30cf31c13
MDJ-NL <mdjleader@gmail.com>
2026-04-29 14:16:33 +0200

    Added local storage functionality & time offline catch-up mechanic.

--- script.js
+++ script.js
@@ -40,6 +40,9 @@ tiredBubble.innerHTML = 'I need rest!';
 unhappyBubble.setAttribute('class', 'alertBubble unhappy');
 unhappyBubble.innerHTML = 'Play with me!';
 
+/* ==============
+    Gamestate
+============== */
 /* selected room
 1 = hunger
 2 = energy
@@ -48,7 +51,6 @@ let currentRoom = 1;
 const feedRoom = '#adccdf';
 const bedRoom = '#addb79';
 const playRoom = '#e6b3cc';
-
 // creatures (unused currently)
 let petSpecies = ['cat', 'dog', 'bunny'] // more added later?
 
@@ -58,7 +60,84 @@ let pet = {
     happiness: 80,
     hungry: false,
     tired: false,
-    unhappy: false
+    unhappy: false,
+    mood: 3, // 0 = run away, 1 = unhappy, 2 = neutral, 3 = happy
+    age: 0,
+    alive: true,
+    name: 'placeholderName'
+}
+
+let tick = 0;
+let saveDate;
+let currentDate = new Date();
+let ToD = 'Daytime'
+let clock = '00:00'
+
+/* ==================
+    game State
+================== */
+const saveToLocalStorage = () => {
+    let petState = JSON.stringify(pet);
+    saveDate = new Date();
+
+    localStorage.setItem('petState', petState);
+    localStorage.setItem('savedDate', saveDate);
+}
+
+// save gamestate on exit
+window.onbeforeunload = function () {
+   saveToLocalStorage();
+}
+
+const loadFromLocalstorage = () => {
+    let petState = localStorage.getItem('petState');
+
+    if (petState === null) {
+        return;
+    } else {
+        pet = JSON.parse(petState);
+    }
+
+    catchUpGameState();
+    updateUI();
+}
+
+const catchUpGameState = () => {
+    let oldSaveDate = new Date(localStorage.getItem('savedDate'));
+
+    if (oldSaveDate === '') return;
+
+    let currentTime = currentDate.getTime();
+    let savedTime = oldSaveDate.getTime();
+
+    let timeDifference = currentTime - savedTime;
+
+    // convert to seconds and round down
+    let catchUpSeconds = Math.floor(timeDifference / 1000);
+
+    for (let i = 0; i < catchUpSeconds; i++) {
+        gameLoop();
+    }
+
+    const notification = new Notification('Page status', {
+        body: `It has been ${Math.floor(catchUpSeconds / 60)} minutes since your last visit. Please take good care of ${pet.name}!`
+    });
+}
+
+const updateTime = () => {
+    currentDate = new Date();
+    let timeOfDay = currentDate.toTimeString();
+
+    let currentHour = timeOfDay.slice(0, 2);
+    clock = timeOfDay.slice(0, 5);
+
+    if (currentHour >=6 || currentHour <= 18) {
+        ToD = 'Daytime';
+    } else if (currentHour <= 22) {
+        ToD = 'Evening';
+    } else {
+        ToD = 'Nighttime';
+    }
 }
 
 /* ======================
@@ -96,6 +175,27 @@ const checkSelection = () => { // add selection to status bars as well
     }
 }
 
+function gameLoop() {
+    tick++;
+
+    if (!pet.alive) return;
+
+    //if (tick % 300 === 0) { // every 5 minutes
+        pet.hunger -= 1;
+    //}
+
+    //if (tick % 180 === 0) { // every 3 minutes
+        pet.energy -= 1;
+    //}
+
+    //if (tick % 240 === 0) { // every 4 minutes
+        pet.happiness -= 1;
+    //}   
+
+    updateTime();
+    updateUI();
+}
+
 /* ==============================
     status bars functionality
 ============================== */
@@ -115,12 +215,6 @@ const updateHungerBar = () => {
     if (pet.hunger <= 0) {
         pet.hunger = 0;
     }
-
-    if (pet.hungry) {
-        bubbleWrapper.appendChild(hungryBubble);
-        } else {
-            hungryBubble.remove();
-    }
 }
 
 const updateEnergyBar = () => {
@@ -138,12 +232,6 @@ const updateEnergyBar = () => {
 
     if (pet.energy <= 0) {
         pet.energy = 0;
-    }
-
-    if (pet.tired) {
-        bubbleWrapper.appendChild(tiredBubble);
-        } else {
-            tiredBubble.remove();
     }
 }
 
@@ -163,6 +251,20 @@ const updateHappinessBar = () => {
     if (pet.happiness <= 0) {
         pet.happiness = 0;
     }
+}
+
+const updateAlerts = () => {
+    if (pet.hungry) {
+        bubbleWrapper.appendChild(hungryBubble);
+        } else {
+            hungryBubble.remove();
+    }
+
+     if (pet.tired) {
+        bubbleWrapper.appendChild(tiredBubble);
+        } else {
+            tiredBubble.remove();
+    }
 
     if (pet.unhappy) {
         bubbleWrapper.appendChild(unhappyBubble);
@@ -175,6 +277,12 @@ const updateStatusbars = () => {
     updateHungerBar();
     updateEnergyBar();
     updateHappinessBar();
+}
+
+const updateUI = () => {
+    updateStatusbars();
+    updateAlerts();
+    checkSelection();
 }
 
 /* =========================
@@ -200,49 +308,31 @@ const pressedRight = () => {
 }
 
 const pressedCenter = () => {
-    if (currentRoom === 1) { // hunger
-        if (pet.hunger >= 100){
-            pet.hunger = 100;
-        } else {
-            pet.hunger += 20;
-        }
+    if (currentRoom === 1) {
+        pet.hunger = Math.min(100, pet.hunger + 20);
 
-    } else if (currentRoom === 2) { // energy
-        if (pet.energy >= 100){
-            pet.energy = 100;
-        } else {
-            pet.energy += 20;
-        }
-       
-    } else if (currentRoom === 3) { // happiness
-        if (pet.happiness >= 100){
-            pet.happiness = 100;
-        } else {
-            pet.happiness += 20;
-        }
-        
-    } else { // debug test for errors, can be deleted later
-        
+    } else if (currentRoom === 2) {
+        pet.energy = Math.min(100, pet.energy + 20);
+
+    } else if (currentRoom === 3) {
+        pet.happiness = Math.min(100, pet.happiness + 20);
     }
+
     updateStatusbars();
 }
 
 btnLeft.addEventListener('click', pressedLeft);
 btnCenter.addEventListener('click', pressedCenter);
 btnRight.addEventListener('click', pressedRight);
-/* ==============================
-    clean start after refresh
-============================== */
+/* ===============
+    first load
+=============== */
 const init = () => {
-    checkSelection();
-    updateStatusbars();
-
-    setInterval(() => { // temporary test timer.
-        pet.hunger -= 5;
-        pet.energy -= 5;
-        pet.happiness -= 5;
-        updateStatusbars();
-    }, 1000);
+    tick = 0;
+    loadFromLocalstorage();
+    updateUI();
+    setInterval(gameLoop, 1000);
+    setInterval(saveToLocalStorage, 300000); // 5min periodic save
 }
 
 init();
